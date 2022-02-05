b3b85e7871942f7cc1f2f3e85acd3fce3e1ca89e
Joe Zedd <zychjoe@gmail.com>
2022-02-05 10:39:55 -0500

    week2

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -7,52 +7,129 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            char[] lowerCase = {'a','b','c','d','e','f','g','h','i','j','k','l',
+                'm','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+            char[] upperCase = {'A','B','C','D','E','F','G','H','I','J','K','L',
+                'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+            foreach (char l in lowerCase)
+            {
+                if (l == c)
+                {
+                    return true;
+                }
+            }
+
+            foreach (char u in upperCase)
+            {
+                if (u == c)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
+
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            return vals.Length % 2 == 0;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            return number % 2 == 0;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return !IsNumberEven(num);
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            double min = double.PositiveInfinity;
+            double max = double.NegativeInfinity;
+
+            foreach(double d in numbers)
+            {
+                if (d < min)
+                {
+                    min = d;
+                }
+
+                if (d > max)
+                {
+                    max = d;
+                }
+            }
+
+            return (double.IsFinite(min) && double.IsFinite(max)) ? min + max : 0;
+            
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            return (str1.Length < str2.Length)? str1.Length: str2.Length;
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if(numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            int counter = 0;
+            foreach(int i in numbers)
+            {
+                counter += i;
+            }
+            return counter;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            int counter = 0;
+            foreach (int i in numbers)
+            {
+                if (IsNumberEven(i))
+                {
+                    counter += i;
+                }
+            }
+            return counter;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count == 0)
+            {
+                return false;
+            }
+            int counter = 0;
+            foreach (int i in numbers)
+            {
+                counter += i;
+            }
+            return IsNumberOdd(counter);
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            if (number <= 1)
+            {
+                return 0;
+            }
+            return (number % 2 == 0)? number / 2 : (number - 1) / 2;
         }
     }
 }
